7e403824339e86850907cdb2abc9e5e99527098f
ben <bentalbot91@gmail.com>
2020-07-04 00:01:23 +1000

    Use token info for custom rendering path

--- src/index.js
+++ src/index.js
@@ -6,15 +6,17 @@ export default class ThoughtDown extends MarkdownIt {
 
   constructor(options) {
     super(options);
+
+    this.renderer.default_rules = Object.assign({}, this.renderer.rules);
+
     this.renderer.rules.fence = function(tokens, idx, options, env, renderer) {
       console.log(tokens[idx]);
-      return '<b>PRETTY GRAPH</b>';
+      if (tokens[idx].info === "gitgraph") {
+        console.log("CUSTOM RENDERING");
+        return '<b>PRETTY GRAPH</b>';
+      } else {
+        return renderer.default_rules.fence(tokens, idx, options, env, renderer);
+      }
     }
   }
 }
-
-export function printMsg() {
-  console.log("This is my message from ThoughtDown");
-}
-
-new ThoughtDown()
