b8e93ed9b0f0cacdcdeb807320e45a5287a1929d
ben <bentalbot91@gmail.com>
2020-07-03 22:04:53 +1000

    Add default class as extension of markdown-it

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
+'use strict';
+
+import MarkdownIt from 'markdown-it';
+
+export default class ThoughtDown extends MarkdownIt {
+
+  constructor(options) {
+    super(options);
+    this.renderer.rules.fence = function(tokens, idx, options, env, renderer) {
+      console.log(tokens[idx]);
+      return '<b>PRETTY GRAPH</b>';
+    }
+  }
+}
+
 export function printMsg() {
   console.log("This is my message from ThoughtDown");
 }
+
+new ThoughtDown()
