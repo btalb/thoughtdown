9a662c084736bba24dca14e30959d93b29cbab2f
Ben Talbot <bentalbot91@gmail.com>
2022-10-10 23:31:10 +1000

    Stop messing around with js bs in tests
    Addresses #10.

--- tests/debug.ts
+++ tests/debug.ts
@@ -1,19 +1,20 @@
 import {readFileSync} from 'fs';
 import {fileURLToPath} from 'url';
 
-import td from '../dist/bundle';
+import a from 'thoughtdown';
+console.log(a());
 
-// import prettier from 'prettier';
+// // import prettier from 'prettier';
 
-import path from 'path';
+// import path from 'path';
 
-const input = readFileSync(path.resolve(__dirname, './debug.md'), {
-  encoding: 'utf8',
-});
-console.log(input);
+// const input = readFileSync(path.resolve(__dirname, './debug.md'), {
+//   encoding: 'utf8',
+// });
+// console.log(input);
 
-const out = `<body>${td.render(input)}</body>`;
+// const out = `<body>${td.render(input)}</body>`;
 
-// console.log(`INPUT MARKDOWN\n\n${input}`);
-// console.log(`RENDERED MARKDOWN:\n\n${prettier.format(out)}`);
-// // console.log(`RENDERED MARKDOWN:\n\n${out}`);
+// // console.log(`INPUT MARKDOWN\n\n${input}`);
+// // console.log(`RENDERED MARKDOWN:\n\n${prettier.format(out)}`);
+// // // console.log(`RENDERED MARKDOWN:\n\n${out}`);
